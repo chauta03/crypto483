3b9ee54cea0334d77d09f385df4026c9666b7c95
tachau03 <taminhchau8903@gmail.com>
2024-04-12 12:41:01 -0400

    create jumbled cipher

--- project1/strong_substi_cipher.py
+++ project1/strong_substi_cipher.py
@@ -1,1 +1,18 @@
 # Goal: instead of shifting the alphabet, we randomly jumbled the letters
+
+import string
+import random
+
+def create_jumbled_cipher():
+    encodings = {}
+    decoding = {}
+    alphabet = list(string.ascii_uppercase)
+    jumbled_alphabet = alphabet.copy()
+    random.shuffle(jumbled_alphabet)
+    for i in range(len(alphabet)):
+        encodings[alphabet[i]] = jumbled_alphabet[i]
+        decoding[jumbled_alphabet[i]] = alphabet[i]
+    return encodings, decoding
+
+def encode(message, subst):
+    return "".join(subst.get(x, x) for x in message)
