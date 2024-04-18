7d656ec73daf94d491e64fe7559033052416aa85
tachau03 <taminhchau8903@gmail.com>
2024-04-17 23:48:53 -0400

    brute force substitution decoder

--- project1/decode.py
+++ project1/decode.py
@@ -1,4 +1,5 @@
 import string
+from itertools import permutations 
 
 # brute force shift cipher decoder
 def brute_force_shift_decoder(plaintext, cipher):
@@ -11,3 +12,19 @@ def brute_force_shift_decoder(plaintext, cipher):
         if decoded == plaintext:
             return shift_dict
     return None
+
+# brute force any substitution cipher decoder
+def brute_force_substitution_decoder(plaintext, cipher):
+    BASE = [i for i in range(26)]
+    perms = permutations(BASE)
+    perms.remove(BASE)
+
+    for perm in perms:
+        substi_dict = {}
+        for i in range(26):
+            substi_dict[string.ascii_uppercase[i]] = i
+        decoded = "".join(substi_dict.get(x, x) for x in cipher)
+        if decoded == plaintext:
+            return substi_dict
+    return None
+
