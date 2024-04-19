d6e0ea6e657201ab9b6c858534c39650dd62d5a1
tachau03 <taminhchau8903@gmail.com>
2024-04-19 17:02:29 -0400

    fix function brute force and add test

--- project1/decode.py
+++ project1/decode.py
@@ -13,18 +13,35 @@ def brute_force_shift_decoder(plaintext, cipher):
             return shift_dict
     return None
 
-# brute force any substitution cipher decoder
+# brute force any substitution cipher decoder (WILL NOT WORK BECAUSE OF TIME LIMIT)
 def brute_force_substitution_decoder(plaintext, cipher):
     BASE = [i for i in range(26)]
-    perms = permutations(BASE)
-    perms.remove(BASE)
+
+    # O(26!)
+    perms = list(permutations(BASE)) 
 
     for perm in perms:
         substi_dict = {}
         for i in range(26):
-            substi_dict[string.ascii_uppercase[i]] = i
+            substi_dict[string.ascii_uppercase[i]] = perm[i]
         decoded = "".join(substi_dict.get(x, x) for x in cipher)
         if decoded == plaintext:
             return substi_dict
     return None
 
+# Test decoder
+def test_decoder():
+    # Test the brute force shift cipher decoder
+    print("Test the brute force shift cipher decoder")
+    plaintext = "HELLO"
+    cipher = "LIPPS"
+    shift_dict = brute_force_shift_decoder(plaintext, cipher)
+    if shift_dict is not None:
+        print("[PASS]")
+        print("Shift dictionary:", shift_dict)
+        print("Decoded message:", "".join(shift_dict.get(x, x) for x in cipher))
+    else:
+        print("[FAIL]")
+
+
+test_decoder()
